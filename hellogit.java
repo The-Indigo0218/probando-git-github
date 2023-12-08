f26085badfde341b05480c886509c4f77f7a6f83
MiguelDev <migopa0222@gmail.com>
2023-12-08 00:22:51 -0500

    Agregamos la suma

--- hellogit.java
+++ hellogit.java
@@ -4,5 +4,9 @@ public class hellogit {
     public static void main(String[] args) {
         System.out.println("HEllO WORLD");
         System.out.println("No se");
+        int n1, n2;
+        n1 = 1;
+        n2 = 3;
+        System.out.println((n1 + n2));
     }
 }
